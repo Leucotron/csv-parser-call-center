db14f8fe16e4bf6629de5c4927f1b5758f5550c3
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-18 16:16:53 -0300

    feat: ensure UploadMailingController return 400 if header is no provided

--- src/presentation/controllers/upload-mailing.ts
+++ src/presentation/controllers/upload-mailing.ts
@@ -2,9 +2,17 @@ import { Controller } from '../contracts/controller'
 import { HttpRequest, HttpResponse } from '../contracts/http'
 export class UploadMailingController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
-    return {
-      statusCode: 400,
-      body: new Error('Field delimiter should be provided')
+    if (!httpRequest.body.delimiter) {
+      return {
+        statusCode: 400,
+        body: new Error('Field delimiter should be provided')
+      }
+    }
+    if (!httpRequest.body.header) {
+      return {
+        statusCode: 400,
+        body: new Error('Field header should be provided')
+      }
     }
   }
 }
