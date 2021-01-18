74683b8344a86d5b86347007d41dcea4be326645
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-18 16:17:11 -0300

    test: ensure UploadMailingController return 400 if header is no provided

--- src/presentation/controllers/upload-mailing.spec.ts
+++ src/presentation/controllers/upload-mailing.spec.ts
@@ -5,7 +5,6 @@ describe('Upload Mailing Controller', () => {
     const sut = new UploadMailingController()
     const httpRequest = {
       body: {
-        delimiter: 'valid_delimiter',
         header: {
           defaultHeader: 'designed_header'
         }
@@ -15,5 +14,18 @@ describe('Upload Mailing Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Field delimiter should be provided'))
+  })
+
+  test('Should return an 400 if no header is provided', () => {
+    const sut = new UploadMailingController()
+    const httpRequest = {
+      body: {
+        delimiter: 'valid_delimiter'
+      },
+      file: 'file_content'
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Field header should be provided'))
   })
 })
