ff71110cfcdfc28b78106566d5c61a7ef2f6e2e1
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-19 15:37:41 -0300

    test: ensure RestAddMailing calls PostAddMailing with correct params

--- src/data/usecases/rest-add-mailing.spec.ts
+++ src/data/usecases/rest-add-mailing.spec.ts
@@ -1,25 +1,59 @@
 import { MailingModel } from '../../domain/models/mailing'
 import { Parser } from '../contracts/parser'
+import { PostAddMailing } from '../contracts/post-add-mailing'
 import { RestAddMailing } from './rest-add-mailing'
 describe('Rest Add Mailing', () => {
   const makeParserStub = (): Parser => {
     class ParserStub implements Parser {
       parse (): MailingModel[] {
-        return null
+        return [
+          {
+            campaignId: 'any_id',
+            cnpj: 'any_cnpj',
+            contactCode: 'any_contact_code',
+            responsibleContact: 'any_responsibleContact',
+            name: 'any_name',
+            cpf: 'any_cpf',
+            email: 'any_email@mail.com',
+            address: {
+              cep: 'any_cep',
+              city: 'any_city',
+              complement: 'any_complement',
+              country: 'any_country',
+              neighborhood: 'any_neighborhood',
+              number: 123,
+              state: 'any_state',
+              street: 'any_street'
+            }
+          }
+        ]
       }
     }
     return new ParserStub()
   }
+
+  const makePostAddMailingStub = (): PostAddMailing => {
+    class PostAddMailingStub implements PostAddMailing {
+      async post (url: string, body: MailingModel[]): Promise<any> {
+        return new Promise(resolve => resolve(null))
+      }
+    }
+    return new PostAddMailingStub()
+  }
+
   interface SutTypes {
     sut: RestAddMailing
     parserStub: Parser
+    postAddMailingStub: PostAddMailing
   }
   const makeSut = (): SutTypes => {
     const parserStub = makeParserStub()
-    const sut = new RestAddMailing(parserStub)
+    const postAddMailingStub = makePostAddMailingStub()
+    const sut = new RestAddMailing(parserStub, postAddMailingStub)
     return {
       sut,
-      parserStub
+      parserStub,
+      postAddMailingStub
     }
   }
   test('Should call Parser with correct params', () => {
@@ -56,5 +90,39 @@ describe('Rest Add Mailing', () => {
     } catch (error) {
       expect(error).toEqual(new Error())
     }
+  })
+
+  test('Should call PostAddMailing with correct params', () => {
+    const { postAddMailingStub, sut } = makeSut()
+    const postSpy = jest.spyOn(postAddMailingStub, 'post')
+    sut.add({
+      campaignId: 1,
+      delimiter: 'any_delimiter',
+      headers: {
+        defaultHeader: 'designed_header'
+      },
+      path: 'any_path'
+    })
+    expect(postSpy).toHaveBeenCalledWith('any_path', [
+      {
+        campaignId: 'any_id',
+        cnpj: 'any_cnpj',
+        contactCode: 'any_contact_code',
+        responsibleContact: 'any_responsibleContact',
+        name: 'any_name',
+        cpf: 'any_cpf',
+        email: 'any_email@mail.com',
+        address: {
+          cep: 'any_cep',
+          city: 'any_city',
+          complement: 'any_complement',
+          country: 'any_country',
+          neighborhood: 'any_neighborhood',
+          number: 123,
+          state: 'any_state',
+          street: 'any_street'
+        }
+      }
+    ])
   })
 })
