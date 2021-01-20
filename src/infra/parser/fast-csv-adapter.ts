68b2425cd6b3373e27721d945825a1cc7db862be
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-20 08:46:20 -0300

    test: ensure FastCSVAdapter return a list of mailings on success

--- src/infra/parser/fast-csv-adapter.ts
+++ src/infra/parser/fast-csv-adapter.ts
@@ -3,33 +3,36 @@ import { MailingModel } from '../../domain/models/mailing'
 import { ParserOptionsArgs, parseFile } from 'fast-csv'
 
 export class FastCSVAdapter implements Parser {
-  parse (path: string, opts: ParserOptionsArgs): MailingModel[] {
-    const mailings = []
-    parseFile<any, MailingModel>(path, opts)
-      .transform((data) => ({
-        name: '',
-        cpf: '',
-        cnpj: '',
-        email: '',
-        address: {
-          street: '',
-          number: 0,
-          cep: '',
-          city: '',
-          complement: '',
-          country: '',
-          neighborhood: '',
-          state: ''
-        },
-        responsibleContact: '',
-        contactCode: '',
-        campaignId: ''
-      }))
-      .on('error', error => {
-        throw error
-      })
-      .on('data', (row: MailingModel) => mailings.push(row))
-      .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`))
-    return mailings
+  async parse (path: string, opts: ParserOptionsArgs): Promise<MailingModel[]> {
+    return new Promise((resolve, reject) => {
+      const mailings = []
+      parseFile<any, MailingModel>(path, opts)
+        .transform((data) => ({
+          name: data.nome,
+          cpf: data.cpf,
+          cnpj: data.cnpj,
+          email: data.email,
+          address: {
+            street: data.endereco,
+            number: data.numero,
+            cep: data.cep,
+            city: data.cidade,
+            complement: data.complemento,
+            country: data.pais,
+            neighborhood: data.bairro,
+            state: data.estado
+          },
+          responsibleContact: 'any_responsible_contact',
+          contactCode: 'any_contact_code',
+          campaignId: 'valid_id'
+        }))
+        .on('error', error => {
+          reject(error)
+        })
+        .on('data', (row: MailingModel) => {
+          mailings.push(row)
+        })
+        .on('end', (rowCount: number) => resolve(mailings))
+    })
   }
 }
