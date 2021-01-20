b82fa1bcf880e7418fca3ce3a5f094233af90c43
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-19 23:11:00 -0300

    feat: ensure FastCSVAdapter calls fast-csv.parseFile with correct values

--- src/infra/parser/fast-csv-adapter.ts
+++ src/infra/parser/fast-csv-adapter.ts
@@ -1,9 +1,11 @@
 import { ParserOptionsArgs } from '@fast-csv/parse'
 import { Parser } from '../../data/contracts/parser'
 import { MailingModel } from '../../domain/models/mailing'
+import * as fc from 'fast-csv'
 
 export class FastCSVAdapter implements Parser {
   parse (path: string, opts: ParserOptionsArgs): MailingModel[] {
+    fc.parseFile(path, opts)
     return null
   }
 }
