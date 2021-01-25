f581621d4097320f5193f3b1e0141806795bcf8b
acdmAnderson <acdm.anderson@hotmail.com>
2021-01-24 22:00:08 -0300

    refactor: change import routes path in production

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,6 +5,7 @@ import env from './env'
 export default (app: Express): void => {
   const router = Router()
   app.use(`${env.context}/api/v1`, router)
-  sync('**/src/main/routes/**routes.ts')
+  const dir = env.isProd ? '**/dist/main/routes/**routes.js' : '**/src/main/routes/**routes.ts'
+  sync(dir)
     .map(async file => (await import(`../../../${file}`)).default(router))
 }
